7c2965147f350e0f85204b6561c599253b4a127e
Peiyuu1219 <tanpy-wm22@student.tarc.edu.my>
2024-09-13 14:35:41 +0800

    Update salaryLR.py

--- salaryLR.py
+++ salaryLR.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import streamlit as st
 from joblib import load
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import Normalizer
 
-# Load the trained model, encoder, and scaler
+# Load the trained model
 model_file = 'LogisticRegression.joblib'
-encoder_file = 'OneHotEncoder.joblib'
-scaler_file = 'Normalizer.joblib'
 
 try:
     model = load(model_file)
-    encoder = load(encoder_file)
-    scaler = load(scaler_file)
+    encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+    scaler = Normalizer()
 except Exception as e:
     st.error(f'Error loading files: {e}')
     st.stop()  # Stop the script if there's an issue with loading files
@@ -29,6 +29,28 @@ native_country_options = ['Cambodia', 'Canada', 'China', 'Columbia', 'Cuba', 'Do
                           'France', 'Germany', 'Greece', 'Guatemala', 'Haiti', 'Holand-Netherlands', 'Honduras', 'Hong', 'Hungary', 'India', 
                           'Iran', 'Ireland', 'Italy', 'Jamaica', 'Japan', 'Laos', 'Mexico', 'Nicaragua', 'Outlying-US(Guam-USVI-etc)', 'Peru', 
                           'Philippines', 'Poland', 'Portugal', 'Puerto-Rico', 'Scotland', 'South', 'Taiwan', 'Thailand', 'Trinadad&Tobago', 'United-States', 'Vietnam', 'Yugoslavia']
+
+# Pad shorter lists to the same length as the longest list (native_country_options)
+max_length = len(native_country_options)
+
+def pad_list(lst, target_length):
+    return (lst * (target_length // len(lst) + 1))[:target_length]
+
+categories_data = {
+    'workclass': pad_list(workclass_options, max_length),
+    'education': pad_list(education_options, max_length),
+    'marital-status': pad_list(marital_status_options, max_length),
+    'occupation': pad_list(occupation_options, max_length),
+    'relationship': pad_list(relationship_options, max_length),
+    'race': pad_list(race_options, max_length),
+    'sex': pad_list(sex_options, max_length),
+    'native-country': native_country_options
+}
+
+dummy_data = pd.DataFrame(categories_data)
+
+# Fit the OneHotEncoder on the categorical columns
+encoder.fit(dummy_data)
 
 def main():
     st.title('Salary Prediction App')
@@ -88,9 +110,7 @@ def main():
             # Predict using the trained model
             prediction = model.predict(final_input_data_scaled)
 
-            # Display the prediction result
-            salary_category = 'greater than $50K' if prediction[0] == 1 else 'less than or equal to $50K'
-            st.success(f'The predicted salary for the provided details is: {salary_category}')
+            st.success(f'The predicted salary for the provided details is: {prediction}')
         except Exception as e:
             st.error(f'An error occurred during prediction: {e}')
 
